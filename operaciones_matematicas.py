a3c699b32193eb498f612f25a944b4763a285572
Tibu100ready <57852407+Tibu100ready@users.noreply.github.com>
2025-01-14 22:37:33 -0400

    feat: pruebas python (#2)

---
+++ operaciones_matematicas.py
@@ -0,0 +1,325 @@
+# a = 6
+# b = 20
+
+# print(a + b)
+# print(a - b)
+# print(a / b) 
+
+# x= str(3)
+# y= int(3)
+# z=float(3)
+
+# print(x)
+# print(y)
+# print(z)
+
+# x=5
+# y='jhon'
+# y="jhon"
+# z=3.14
+# aa=5,50
+# ab=['adrian, pepe, ricardo, gonzalo']
+# ac={2,3,4}
+# X="Hola Mundo"
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(aa))
+# print(type(ab))
+# print(type(ac))
+# print(X)
+
+# """
+# hola
+# """
+
+# print ("""hola""")
+
+# myvar,xa,ya ="jhon",'papita','mani'
+
+# print(myvar)
+# print(xa)
+# print(ya)
+# print(type(myvar))
+# print(type(xa))
+# print(type(ya))
+
+# myvar=xa=ya ="jhon",'papita','mani'
+
+# print(myvar)
+# print(xa)
+# print(ya)
+# print(type(myvar))
+# print(type(xa))
+# print(type(ya))
+
+# x = 5
+# y = "Bs."
+# aa = 1
+# ab = "USD"
+# print(y, x)
+# print(ab, aa)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python.py
@@ -0,0 +1,30 @@
+
+
+for fruta in "x":
+    print(fruta)
+
+# limite = int(input('Ingrese el limite de tiempo'))
+# for i in range(limite):
+#     print("numero: ",i+1)
+
+# filas = int(input('ingrese el numero de filas'))
+# columnas = int(input('ingrese el numero de columnas'))
+# for i in range(filas):
+#     for j in range(columnas):
+#         print('fila:','i') 'columna': ,'j')
+
+matriz =[
+[1,2,3],
+[4,5,6],
+[7,8,9]]
+
+for i in range(3):
+    for j in range (3):
+        print('fila:',i+1,'columna',j+1,'valor:',matriz[i][j])
+
+# for i in range(3):
+#     for j in range (3):
+#         if(i==j):
+#         print(matriz[i][j])
+              
+            
