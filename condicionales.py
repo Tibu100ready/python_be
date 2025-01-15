0034d002f0a38d9eddb8a1fdfc2fce19f986d4f0
Your Name <vicjavrodcol@gmail.com>
2025-01-15 00:15:14 -0400

    feat: avance py

---
+++ condicionales.py
@@ -0,0 +1,38 @@
+# edad=17
+
+# if edad >= 18 and edad <=59:
+#     print('"eres mayor de edad"')
+# elif edad > 59:
+#     print('"eres un adulto mayor"')
+# else:
+#     print('"no vas a pasar"')    
+# print("'esto se ejecuta siempre'")
+
+# edad=9.6
+
+# if edad >= 0:
+#     if edad > 10:
+#         if edad >= 18:
+#             print("eres mayor de edad")
+#         print("eres joven")
+#     print("eres humano")
+
+edad=int(input("data "))
+if edad >= 18:
+    print("aceptar")
+else:
+    print("rechazar")
+
+condicion=input("Eres gay? (si/no): ")
+
+while condicion != "no":
+
+    edad=int(input("cagada "))
+    if edad >= 18:
+        print("aceptar")
+    else:
+        print("rechazar")
+    
+    condicion=input("Deseas seguir? (si/no): ")
+   
+
